ea0f22f822cc7b3885b62489562396df7e3fe1a3
Italo de Assis <italomachado8000@gmail.com>
2024-02-27 18:52:38 -0300

    aula 27

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/home.html', context={'name': 'italo',})
 
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-def sobre(request):
-    return HttpResponse('sobre')
 
 
 
