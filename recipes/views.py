36a5b268dd737df4dc3443f198d343a6fef04fa7
Italo de Assis <italomachado8000@gmail.com>
2024-02-29 20:57:46 -0300

    aula 29 a 32

--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/home.html', context={'name': 'italo',})
+    return render(request, 'recipes/pages/home.html', context={'name': 'italo',})
 
 
 
